785591121d406c30fb95d4c08e9c1aa7925c73bb
phy122 <qkrghdud369@gmail.com>
2024-12-03 17:19:49 +0900

    asdas

--- freeorder/src/main/java/com/aloha/freeorder/mapper/CategoryMapper.java
+++ freeorder/src/main/java/com/aloha/freeorder/mapper/CategoryMapper.java
@@ -1,5 +1,22 @@
 package com.aloha.freeorder.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.aloha.freeorder.domain.Category;
+
+@Mapper
 public interface CategoryMapper {
+
+     public List<Category> list() throws Exception;
+
+    public Category read(Long id) throws Exception;
+
+    public int insert(Category category) throws Exception;
+
+    public int update(Category category) throws Exception;
+
+    public int delete(Long id) throws Exception;
     
 }
