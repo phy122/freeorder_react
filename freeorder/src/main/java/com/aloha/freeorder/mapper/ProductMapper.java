4c58a0e6d62688df155e1c71ce83764e44ba91fb
bsy7021 <bsy7021@naver.com>
2024-12-03 17:50:33 +0900

    수정 전

--- freeorder/src/main/java/com/aloha/freeorder/mapper/ProductMapper.java
+++ freeorder/src/main/java/com/aloha/freeorder/mapper/ProductMapper.java
@@ -2,10 +2,19 @@ package com.aloha.freeorder.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.aloha.freeorder.domain.Product;
 
+@Mapper
 public interface ProductMapper {
     
-    public List<Product> list(Product product) throws Exception;
-    
+    public List<Product> list() throws Exception;
+
+    public int insert(Product product) throws Exception;
+
+    public int update(Product product) throws Exception;
+
+    public int delete(String id) throws Exception;
+
 }
--- freeorder/src/main/java/com/aloha/freeorder/service/ProductService.java
+++ freeorder/src/main/java/com/aloha/freeorder/service/ProductService.java
@@ -1,5 +1,14 @@
 package com.aloha.freeorder.service;
 
+import java.util.List;
+
+import com.aloha.freeorder.domain.Product;
+
 public interface ProductService {
-    
+
+    public List<Product> list() throws Exception;
+    public int insert(Product product) throws Exception;
+    public int update(Product product) throws Exception;
+    public int delete(String id) throws Exception;
+
 }
