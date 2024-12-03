4b0b7db5db193f7ab31a6fff133c03a5de1b4527
bsy7021 <bsy7021@naver.com>
2024-12-03 16:59:04 +0900

    1

--- freeorder/src/main/java/com/aloha/freeorder/mapper/ProductMapper.java
+++ freeorder/src/main/java/com/aloha/freeorder/mapper/ProductMapper.java
@@ -1,5 +1,11 @@
 package com.aloha.freeorder.mapper;
 
+import java.util.List;
+
+import com.aloha.freeorder.domain.Product;
+
 public interface ProductMapper {
+    
+    public List<Product> list(Product product) throws Exception;
     
 }
