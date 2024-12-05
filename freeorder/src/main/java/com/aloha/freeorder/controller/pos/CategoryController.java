d1907ecc66549ffba22e1ec9f939dabc74db6141
bsy7021 <bsy7021@naver.com>
2024-12-05 12:34:13 +0900

    카테고리, 카트, 결제 컨트롤러 수정

--- freeorder/src/main/java/com/aloha/freeorder/controller/pos/CategoryController.java
+++ freeorder/src/main/java/com/aloha/freeorder/controller/pos/CategoryController.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aloha.freeorder.domain.Category;
 import com.aloha.freeorder.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 /**
@@ -20,27 +23,31 @@ import org.springframework.web.bind.annotation.RequestParam;
  * CRUD 비동기 처리
  * 
  */
+@Slf4j
 @RestController
-@RequestMapping("/pos/category")
+@RequestMapping("/pos/categories")
 public class CategoryController {
     
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("/list")
+    @GetMapping()
     public ResponseEntity<?> list() throws Exception {
+        log.info("카테고리 목록 조회");
         List<Category> CategoryList = categoryService.list();
         return ResponseEntity.ok(CategoryList);
     }
 
-    @GetMapping("/read")
-    public ResponseEntity<?> read(@RequestParam("id") Long id) throws Exception {
+    @GetMapping("/{id}")
+    public ResponseEntity<?> read(@PathVariable("id") Long id) throws Exception {
+        log.info("카테고리 조회");
         Category category = categoryService.read(id);
         return ResponseEntity.ok(category);
     }
 
-    @PostMapping("/insert")
-    public ResponseEntity<?> insertPro(@RequestBody Category category) throws Exception {
+    @PostMapping()
+    public ResponseEntity<?> insertPro(Category category) throws Exception {
+        log.info("카테고리 등록");
         int result = categoryService.insert(category);
         if (result > 0) {
             return ResponseEntity.ok("success");
@@ -48,8 +55,9 @@ public class CategoryController {
         return ResponseEntity.badRequest().body("error");
     }
 
-    @PostMapping("/update")
-    public ResponseEntity<?> updatePro(@RequestBody Category category) throws Exception {
+    @PutMapping()
+    public ResponseEntity<?> updatePro(Category category) throws Exception {
+        log.info("카테고리 수정");
         int result = categoryService.update(category);
         if(result > 0){
             return ResponseEntity.ok("seccess");
@@ -57,8 +65,9 @@ public class CategoryController {
         return ResponseEntity.badRequest().body("error");
     }
 
-    @PostMapping("/delete")
-    public ResponseEntity<?> delete(@RequestParam("id") Long id) throws Exception {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) throws Exception {
+        log.info("카테고리 삭제");
         int result = categoryService.delete(id);
         if(result > 0){
             return ResponseEntity.ok("seccess");
