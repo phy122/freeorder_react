48f73528c9f35d90f4100d9baf325eefcbb3e511
bsy7021 <bsy7021@naver.com>
2024-12-09 15:31:43 +0900

    상품 등록 수정 컨트롤러, 매퍼 수정

--- freeorder/src/main/java/com/aloha/freeorder/service/PaymentService.java
+++ freeorder/src/main/java/com/aloha/freeorder/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.aloha.freeorder.service;
 
 import java.util.List;
 
+import com.aloha.freeorder.domain.PaySearch;
 import com.aloha.freeorder.domain.Payment;
 
 public interface PaymentService {
@@ -9,6 +10,8 @@ public interface PaymentService {
     public Payment select(String id) throws Exception;
     // 목록
     public List<Payment> list() throws Exception;
+    // 목록 검색
+    public List<Payment> listByOption(PaySearch paySearch) throws Exception;
     // 등록
     public int insert(Payment payment) throws Exception;
     // 수정
