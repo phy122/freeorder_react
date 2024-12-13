47799e282c5860366bb65a9fb9d33459804660c6
dojinss <ehwls20@gmail.com>
2024-12-13 11:58:56 +0900

    오류 수정

--- freeorder/src/main/java/com/aloha/freeorder/service/CartService.java
+++ freeorder/src/main/java/com/aloha/freeorder/service/CartService.java
@@ -26,4 +26,21 @@ public interface CartService {
     public int allDeleteByUserId(String usersId) throws Exception;
     // 결제시 해당 내역 장바구니 옵션 삭제
     public int allDeleteOptionByUserId(String usersId) throws Exception;
+    // 
+    // 장바구니 옵션 비교
+    public static boolean areOptionListsEqual(List<CartOption> list1, List<CartOption> list2) {
+        if (list1.size() != list2.size()) {
+            return false; // 크기가 다르면 다른 옵션 세트
+        }
+
+        // 각 옵션의 ID를 기준으로 비교 (정렬이 다를 수 있으니 정렬 후 비교)
+        return list1.stream()
+                .map(CartOption::getOptionItemsId)
+                .sorted()
+                .toList()
+                .equals(list2.stream()
+                             .map(CartOption::getOptionItemsId)
+                             .sorted()
+                             .toList());
+    }
 }
