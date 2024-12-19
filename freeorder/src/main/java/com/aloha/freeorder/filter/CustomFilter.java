c447dbc67076cafac5bb95dc40d7fc6c529126c4
dojinss <ehwls20@gmail.com>
2024-12-19 15:29:43 +0900

    필터 수정

--- freeorder/src/main/java/com/aloha/freeorder/filter/CustomFilter.java
+++ freeorder/src/main/java/com/aloha/freeorder/filter/CustomFilter.java
@@ -3,9 +3,7 @@ package com.aloha.freeorder.filter;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jms.JmsProperties.Listener.Session;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.aloha.freeorder.domain.SystemStatus;
@@ -28,15 +26,15 @@ public class CustomFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain)
             throws ServletException, IOException {
-        // 세션
-        HttpSession session = request.getSession();
-        String usersId = (String) session.getAttribute("id");
-        if (usersId.equals("") || usersId == null) {
-            response.sendRedirect("/qr/main");
-        }
         // 현재경로
         String requestURI = request.getRequestURI();
         if (requestURI.contains("/qr")) { // /qr 경로에만 적용
+            // 세션
+            HttpSession session = request.getSession();
+            String usersId = (String) session.getAttribute("id");
+            if (usersId == null || usersId.equals("")) {
+                response.sendRedirect("/qr/main");
+            }
             // statusService 에서 서버 상태 불러오기
             SystemStatus systemStatus = statusService.selectStatus();
             String status = systemStatus.getStatus();
