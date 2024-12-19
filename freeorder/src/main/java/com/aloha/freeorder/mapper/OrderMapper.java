ba7e85078b08619ff9182ec25c492d35ca7ef8f6
phy122 <qkrghdud369@gmail.com>
2024-12-19 19:19:27 +0900

    제발...너무 힘들어요...

--- freeorder/src/main/java/com/aloha/freeorder/mapper/OrderMapper.java
+++ freeorder/src/main/java/com/aloha/freeorder/mapper/OrderMapper.java
@@ -3,6 +3,7 @@ package com.aloha.freeorder.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.aloha.freeorder.domain.Order;
 import com.aloha.freeorder.domain.OrderItem;
@@ -14,27 +15,37 @@ public interface OrderMapper {
     
     // 주문 목록
     public List<Order> list() throws Exception;
+    
     // 주문 목록(상태)
     public List<Order> listByStatus(String status) throws Exception;
+    
     // 주문 목록(유저아이디)
     public List<Order> listByUsersId(String usersId) throws Exception;
+    
     // 주문 조회
     public Order read(String id) throws Exception;
+    
     // 주문 등록
     public int insert(Order order) throws Exception;
+    
     // 아이템 등록
-    public int insertItem(OrderItem OrderItem) throws Exception;
+    public int insertItem(OrderItem orderItem) throws Exception;
+    
     // 옵션 등록
     public int insertOption(OrderOption orderOption) throws Exception;
+    
     // 주문 수정
     public int update(Order order) throws Exception;
+    
     // 주문 삭제
     public int delete(String id) throws Exception;
+    
     // 월별 매출 조회
-    public SalesReport totalMonth(String status, String day) throws Exception;
+    public SalesReport totalMonth(@Param("status") String status, @Param("day") String day);
+    
     // 일별 매출 조회
-    public SalesReport totalDay(String status, String day) throws Exception;
+    public SalesReport totalDay(@Param("status") String status, @Param("day") String day);
+    
     // 주문수 조회(주문번호 부여)
     public int countOrders() throws Exception;
-
 }
