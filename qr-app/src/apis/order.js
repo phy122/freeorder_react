193b1660d282ed43408113856906305fa7134796
Yoo-JiHyun <luhyun26@gmail.com>
2025-01-16 10:43:05 +0900

    QR API

--- qr-app/src/apis/order.js
+++ qr-app/src/apis/order.js
@@ -3,17 +3,17 @@ import axios from "axios"
 axios.defaults.baseURL = "/api"
 
 // 목록
-export const list = () => axios.get(`/`)
+export const list = () => axios.get(`/qr/orders`)
 
 // 조회
-export const select = (id) => axios.get(`//${id}`)
+export const select = (id) => axios.get(`/qr/orders/${id}`)
 
 // 등록
 // export const insert = (title, writer, content) => axios.post("boards", {title, writer, content})
-export const insert = (formData, headers) => axios.post("/", formData, headers)
+export const insert = (formData, headers) => axios.post("/qr/orders", formData, headers)
 
 // 수정
-export const update = (formData, headers) => axios.put("/", formData, headers)
+export const update = (formData, headers) => axios.put("/qr/orders", formData, headers)
 
 // 삭제
-export const remove = (id) => axios.delete(`//${id}`)
+export const remove = (id) => axios.delete(`/qr/orders/${id}`)
