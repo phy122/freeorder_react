7ff988fdc52e110f179190cbfbe2dc01b88d0a04
phy122 <qkrghdud369@gmail.com>
2025-01-17 10:55:27 +0900

    옵션 리스트 변환

--- pos-app/src/containers/option/OptionContainer.jsx
+++ pos-app/src/containers/option/OptionContainer.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import OptionList from '../../components/option/OptionList'
+import * as options from '../../apis/option'
 
 const OptionContainer = () => {
+
+  const [optionList, setOptionList] = useState([])
+
+  // 옵션 목록 불러오기
+  const optionLoad = async () => {
+    try {
+      const response = await options.list()
+      const data = response.data
+      const status = response.status
+      if(status == 200){
+        setOptionList(data)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    optionLoad()
+    return () => {}
+  }, [])
+  
   return (
     <>
-    <OptionList />
+    <OptionList
+      optionList={optionList}
+    />
     </>
   )
 }
--- pos-app/src/containers/option/OptionInsertContainer.jsx
+++ pos-app/src/containers/option/OptionInsertContainer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import OptionInsert from '../../components/option/OptionInsert'
+import { useNavigate } from 'react-router-dom'
 
 const OptionInsertContainer = () => {
+
+  const navigate = useNavigate()
+
+  const optionInsert = async (formData, headers) => {
+    try {
+      const response = await option.insert(formData, headers)
+      const data = await response.data
+      console.log(data)
+      alert('등록 완료')
+      navigate('/options')
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <>
-    <OptionInsert />
+    <OptionInsert
+      optionInsert={optionInsert}
+    />
     </>
   )
 }
